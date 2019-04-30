68b73cf2b5987e2f0ea73ebdcb46c1189b9e446a
zhangfenghua68 <zhangfenghua@colont.com>
2019-04-30 11:32:15 +0800

    添加dubbo支持

--- src/main/java/com/dy/learn/learn/LearnApplication.java
+++ src/main/java/com/dy/learn/learn/LearnApplication.java
@@ -4,6 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -14,6 +16,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @MapperScan("com.dy.learn.learn.dao.mapper")
 @EnableScheduling
+//@ImportResource({"classpath:applicationContext.xml"})
+//@PropertySource({"classpath:/conf/zookeeper.properties"})
 public class LearnApplication {
 
 	public static void main(String[] args) {
